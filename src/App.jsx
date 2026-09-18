f945758da399330999fd32c5e3e8fa33c55bb920
Kudratov1412 <furqatkudratov545@gmail.com>
2026-09-19 02:45:48 +0500

    5655

--- src/App.jsx
+++ src/App.jsx
@@ -60,7 +60,7 @@ const App = () => {
     const queryID = telegram.initDataUnsafe?.query_id;
 
     if (queryID) {
-      fetch("https://furqat-telegram-bot-8e38c0bf34c0.herokuapp.com/", {
+      fetch("https://furqat-telegram-bot-8e38c0bf34c0.herokuapp.com/web-data", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
