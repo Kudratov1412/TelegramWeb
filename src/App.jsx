20c2a4a2750741edf18f4766f0fd47fcf3a379c8
Kudratov1412 <furqatkudratov545@gmail.com>
2026-09-19 14:41:59 +0500

    fgdsgf

--- src/App.jsx
+++ src/App.jsx
@@ -55,6 +55,8 @@ const App = () => {
   const onCheckout = () => {
     telegram.MainButton.text = "Sotib olish :)";
     telegram.MainButton.show();
+
+    console.log(cartItems);
   };
 
   const onSendData = useCallback(() => {
