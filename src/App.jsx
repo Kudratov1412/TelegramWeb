ce496d9cfdaf2420f2678439d63c4de877ca2520
Kudratov1412 <furqatkudratov545@gmail.com>
2026-09-23 01:09:44 +0500

    bfvssdfs

--- src/App.jsx
+++ src/App.jsx
@@ -95,7 +95,7 @@ const App = () => {
     <>
       <h1 className="heading">Furqatning kurslari</h1>
       <Cart cartItems={cartItems} onCheckout={onCheckout} />
-      {check}
+      <h3>{check}</h3>
       <div className="cards__container">
         {courses.map((course) => (
           <Card
